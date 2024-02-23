a6a8caa8c755298165deb4bfd696ee375f7b3a07
Irfan K <unknown7k7k@gmail.com>
2024-02-23 21:53:34 +0530

    uupdated animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,18 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className="relative overflow-x-hidden">
+    <div className="relative overflow-x-clip">
       {/* <Landing /> */}
       {/* <h1>three js </h1> */}
       {/* <ThreeScreen /> */}
       {/* <Canvas /> */}
       <Hero />
-      <div className="block h-[150vh] w-screen bg-[#1d1d1d]"></div>
+      <div className="relative block h-[150vh] w-screen bg-[#1d1d1d] mix-blend-screen"></div>
       {/* <ProjectContainer /> */}
 
      <Detail />
 
+      <div className="relative block h-[450vh] w-screen bg-[#1d1d1d] text-slate-50 mix-blend-screen">asdf</div>
     </div>
   );
 };
--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,27 +1,97 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Icon from "./Icon";
-import { useScroll } from "framer-motion";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 const Detail = () => {
-  const {} = useScroll()
+  const detailContainerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: detailContainerRef,
+    offset: ["start end", "end 0.6"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0.1, 0.95], [0.1, 15]);
+  const rotate = useTransform(
+    scrollYProgress,
+    [0.1, 0.9, 0.9, 1],
+    [0, 225, 225, 225]
+  );
+
+  const svgTransalteY = useTransform(
+    scrollYProgress,
+    [0.95, 0.951],
+    ["0%", "-300%"]
+  );
+  const backgrondEffectZindex = useTransform(
+    scrollYProgress,
+    [0.945, 0.946],
+    [-1, 4]
+  );
+
+  const textTranslate = useTransform(
+    scrollYProgress,
+    [0.02, 0.91],
+    ["50%", "-50%"]
+  );
+
+  const wordTopTransalte = useTransform(
+    scrollYProgress,
+    [0.92, 0.95],
+    ["0%", "-100%"]
+  );
+  const wordBottomTransalte = useTransform(
+    scrollYProgress,
+    [0.92, 0.95],
+    ["0%", "100%"]
+  );
 
   return (
-    <div className=" w-full h-screen bg-[#1d1d1d]  ">
+    <div
+      className=" w-full   bg-[#1d1d1d] text-slate-50 "
+      ref={detailContainerRef}
+    >
       <div className=" w-full flex items-center justify-center mix-blend-lighten">
         <div className="relative w-screen">
-          <div className=" w-full h-screen flex items-center justify-center aspect-square z-[2] bg-black mix-blend-multiply">
-            <div className="w-[30%] aspect-square">
-              <Icon />
-            </div>
+          <div className="relative w-screen h-[700vh] flex flex-col  z-[2] items-start bg-black mix-blend-multiply">
+            <motion.div className="sticky top-[5vh]  h-fit w-[30vw] aspect-square  mx-auto block">
+              <motion.div style={{ scale, rotate, translateY: svgTransalteY }}>
+                <Icon />
+              </motion.div>
+              {/* <div className="relative"> */}
+              <motion.div
+                className="absolute gap-6 flex  top-[60%]  left-[15vw] translate-y-[-50%] text-[#1d1d1d] text-5xl w-fit text-nowrap font-semibold"
+                style={{ translateX: textTranslate }}
+              >
+                {"WHERE DIFFERENT IS THE STANDARD. CHOOSE OF+BRAND"
+                  .split(" ")
+                  .map((elm, index) => {
+                    return (
+                      <div className="overflow-hidden" key={index}>
+                        <motion.div
+                          style={{
+                            translateY:
+                              index % 2 == 0
+                                ? wordTopTransalte
+                                : wordBottomTransalte,
+                          }}
+                        >
+                          {elm}
+                        </motion.div>
+                      </div>
+                    );
+                  })}
+              </motion.div>
+              {/* </div> */}
+            </motion.div>
           </div>
 
-          <div
-            className="absolute top-0 w-full h-screen  "
+          <motion.div
+            className="absolute top-0 w-full h-full  "
             style={{
               backgroundImage:
                 "linear-gradient(  255deg,  #facb0e,  #f06ba8 30%,  #78bae6 65%,  #fff)",
-              zIndex: "-1",
+              zIndex: backgrondEffectZindex,
+              borderRadius: "1.5%",
             }}
           >
             <div
@@ -30,9 +100,10 @@ const Detail = () => {
                 background:
                   "linear-gradient(  255deg,  #f06ba8,  #facb0e 30%,  #78bae6 65%,  #fff)",
                 animation: "bgAnim 5s infinite",
+                borderRadius: "1.5%",
               }}
             ></div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
